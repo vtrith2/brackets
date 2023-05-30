654251943f69e88e65d2f1220a989f6c006cc81e
vtrith2 <v.trith2@cloudhms.net>
2023-05-30 11:55:05 +0700

    Update something

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Stack;
 public class Main implements ITextFile {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<String>();
-        String openBracket = "([{<";
-        String closeBracket = ")]}>";
+        String openBrackets = "([{<";
+        String closeBrackets = ")]}>";
 
         String brackets = ITextFile.getDataFromFile("src/data.txt");
 
         boolean result = true;
         for (int i = 0; i < brackets.length(); i++) {
-            boolean isOpenBracket = openBracket.contains(String.valueOf(brackets.charAt(i)));
+            boolean isOpenBracket = openBrackets.contains(String.valueOf(brackets.charAt(i)));
 
             if (isOpenBracket) {
                 stack.push(String.valueOf(brackets.charAt(i)));
             } else {
                 String top = stack.pop();
-                int indexOfTop = openBracket.indexOf(top);
-                int indexOfCurrent = closeBracket.indexOf(brackets.charAt(i));
+                int indexOfTop = openBrackets.indexOf(top);
+                int indexOfCurrent = closeBrackets.indexOf(brackets.charAt(i));
                 if (indexOfTop != indexOfCurrent) {
                     result = false;
                     break;
